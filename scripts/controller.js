b1fe6f7571e7c8af62b2592db375cd4990b1c80e
Ben Klinovsky <bnk989@gmail.com>
2024-02-28 17:59:07 +0200

    Improve js files distribution

--- scripts/controller.js
+++ scripts/controller.js
@@ -45,12 +45,12 @@ function addListeners() {
     e.preventDefault()
     loadImageFromInput(e)
   })
-}
 
-window.addEventListener('resize', function () {
-  gCanvas.width = window.innerWidth / 2
-  gCanvas.height = window.innerHeight - 160
-})
+  window.addEventListener('resize', function () {
+    gCanvas.width = window.innerWidth / 2
+    gCanvas.height = window.innerHeight - 160
+  })
+}
 
 function setImage(elImg) {
   gCurrImg = elImg
@@ -61,66 +61,9 @@ function setImage(elImg) {
   gTexts.forEach((text) => text.writeText())
 }
 
-class Text {
-  #ctx
-  constructor(str, x = 20, y = 60, fontSize = 30, fontFam = 'Impact') {
-    this.#ctx = gCtx
-    this.str = str
-    this.pos = { x: x, y: y }
-    this.rectPos = { x: this.x - 10, y: this.y - this.fontSize }
-    this.fontSize = fontSize
-    this.#ctx.font = `${this.fontSize}px ${this.fontFam}`
-    this.width = this.#ctx.measureText(this.str).width + 20
-    this.height = this.fontSize * 1.2
-    this.reSizeDot = { x: this.width * 1.03, y: this.fontSize * 2.2 }
-    this.r = 6
-  }
-
-  writeText() {
-    this.#ctx.font = `${this.fontSize}px Impact`
-    this.#ctx.fillStyle = 'white'
-    this.#ctx.lineWidth = 5
-    this.#ctx.strokeText(this.str, this.pos.x, this.pos.y)
-    this.#ctx.fillText(this.str, this.pos.x, this.pos.y)
-    this.height = this.fontSize * 1.2
-    this.width = this.#ctx.measureText(this.str).width + 20
-    this.#ctx.lineWidth = 1
-  }
-
-  makeRectAround() {
-    this.rectPos = { x: this.pos.x - 10, y: this.pos.y - this.fontSize }
-    this.#ctx.setLineDash([10])
-    this.#ctx.beginPath()
-    this.#ctx.rect(this.rectPos.x, this.rectPos.y, this.width, this.height)
-    this.#ctx.stroke()
-    this.reSizeDot.x = this.rectPos.x + this.width
-    this.reSizeDot.y = this.rectPos.y + this.height
-    this.makeResizeDot()
-  }
-
-  makeResizeDot() {
-    this.#ctx.beginPath()
-    this.#ctx.setLineDash([1])
-    this.#ctx.arc(this.reSizeDot.x, this.reSizeDot.y, this.r, 0, 2 * Math.PI)
-    this.#ctx.closePath()
-    this.makeResizeTriangle()
-  }
-
-  makeResizeTriangle() {
-    this.#ctx.beginPath()
-    this.#ctx.moveTo(this.reSizeDot.x, this.reSizeDot.y)
-    this.#ctx.lineTo(this.reSizeDot.x, this.reSizeDot.y - 12)
-    this.#ctx.lineTo(this.reSizeDot.x - 12, this.reSizeDot.y)
-    this.#ctx.closePath()
-    this.#ctx.fill()
-    this.#ctx.stroke()
-  }
-}
-
-let mouseMove = (e) => {
+const mouseMove = (e) => {
   e.preventDefault()
   const pos = getEventPos(e)
-  //gTexts.find((text, idx) => {
     for (let idx = 0; idx < gTexts.length; idx++) {
     if (isMouseOnRect(pos.x, pos.y, gTexts[idx])) {
       gCanvas.onmousedown = onDown
@@ -144,7 +87,7 @@ let mouseMove = (e) => {
       gCanvas.style.cursor = 'initial'
       gCanvas.onmousedown = null
     }
-  }//)
+  }
 }
 
 const onDown = (e) => {
--- scripts/services/canvas-edits.service.js
+++ scripts/services/canvas-edits.service.js
@@ -8,6 +8,62 @@ var gMeme = {
 }
 var gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
 
+class Text {
+  #ctx
+  constructor(str, x = 20, y = 60, fontSize = 30, fontFam = 'Impact') {
+    this.#ctx = gCtx
+    this.str = str
+    this.pos = { x: x, y: y }
+    this.rectPos = { x: this.x - 10, y: this.y - this.fontSize }
+    this.fontSize = fontSize
+    this.#ctx.font = `${this.fontSize}px ${this.fontFam}`
+    this.width = this.#ctx.measureText(this.str).width + 20
+    this.height = this.fontSize * 1.2
+    this.reSizeDot = { x: this.width * 1.03, y: this.fontSize * 2.2 }
+    this.r = 6
+  }
+
+  writeText() {
+    this.#ctx.font = `${this.fontSize}px Impact`
+    this.#ctx.fillStyle = 'white'
+    this.#ctx.lineWidth = 5
+    this.#ctx.strokeText(this.str, this.pos.x, this.pos.y)
+    this.#ctx.fillText(this.str, this.pos.x, this.pos.y)
+    this.height = this.fontSize * 1.2
+    this.width = this.#ctx.measureText(this.str).width + 20
+    this.#ctx.lineWidth = 1
+  }
+
+  makeRectAround() {
+    this.rectPos = { x: this.pos.x - 10, y: this.pos.y - this.fontSize }
+    this.#ctx.setLineDash([10])
+    this.#ctx.beginPath()
+    this.#ctx.rect(this.rectPos.x, this.rectPos.y, this.width, this.height)
+    this.#ctx.stroke()
+    this.reSizeDot.x = this.rectPos.x + this.width
+    this.reSizeDot.y = this.rectPos.y + this.height
+    this.makeResizeDot()
+  }
+
+  makeResizeDot() {
+    this.#ctx.beginPath()
+    this.#ctx.setLineDash([1])
+    this.#ctx.arc(this.reSizeDot.x, this.reSizeDot.y, this.r, 0, 2 * Math.PI)
+    this.#ctx.closePath()
+    this.makeResizeTriangle()
+  }
+
+  makeResizeTriangle() {
+    this.#ctx.beginPath()
+    this.#ctx.moveTo(this.reSizeDot.x, this.reSizeDot.y)
+    this.#ctx.lineTo(this.reSizeDot.x, this.reSizeDot.y - 12)
+    this.#ctx.lineTo(this.reSizeDot.x - 12, this.reSizeDot.y)
+    this.#ctx.closePath()
+    this.#ctx.fill()
+    this.#ctx.stroke()
+  }
+}
+
 function isMouseOnRect(x, y, text) {
   return (
     x >= text.rectPos.x - gExpandArea &&
